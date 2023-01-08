572842912edaaf0b7a9e7c47c33bcec11313453a
Jexytd <naufalwafi0104@gmail.com>
2023-01-08 07:38:03 +0700

    Add IsOwn

--- SimpleCode.lua
+++ SimpleCode.lua
@@ -23,18 +23,16 @@ function SIMP:NewInstance(instance, properties, children)
     end
 
     local children = children or {}
-    if #children > 0 then
-        for k, v in pairs(children) do
-            if typeof(v) == 'Instance' then
-                v.Parent = instances
-            end
+    for k, v in pairs(children) do
+        if typeof(v) == 'Instance' then
+            v.Parent = instances
         end
     end
 
     return instances;
 end
 
-function SIMP:ChangeProperties(instance, newProperties)
+function SIMP:ChangeProperties(instance, newProperties, children)
     assert(instance, 'The instance are nil, not able to continue.')
     assert(newProperties, 'Please insert properties to changed, on table!')
 
@@ -42,7 +40,23 @@ function SIMP:ChangeProperties(instance, newProperties)
         instance[k] = v;
     end
 
+    local children = children or {};
+    for k,v in pairs(children) do
+        if typeof(v) == 'Instance' then
+            v.Parent = instance;
+        end
+    end
+
     return instance
+end
+
+function SIMP:IsOwn(tab, value)
+    for k,v in pairs(tab) do
+        if k == value or v == value then
+            return v;
+        end
+    end
+    return;
 end
 
 return SIMP;
