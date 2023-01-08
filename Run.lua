a9099bb0966875fdb25e07a8506b077840dca4a3
Jexytd <naufalwafi0104@gmail.com>
2023-01-08 11:14:49 +0700

    Add new way on Authenticator

--- Run.lua
+++ Run.lua
@@ -4,14 +4,57 @@ repeat task.wait() until game:IsLoaded();
 local SIMP = loadstring(game:HttpGet('https://raw.githubusercontent.com/Jexytd/kodok/main/SimpleCode.lua'))()
 
 --/ Authenticator /--
-local ResponseKey = loadstring(SIMP:Get('https://raw.githubusercontent.com/Jexytd/kodok/main/Auth.lua'))()
-if ResponseKey == 1 then
-    return 0;
-elseif ResponseKey == -1 then
-    return 1;
-end
+local Auth = loadstring(SIMP:Get('https://raw.githubusercontent.com/Jexytd/kodok/main/Auth.lua'))()
 
-print('Key checker passed!')
+--/ UI /--
+local UI = loadstring(SIMP:Get('https://raw.githubusercontent.com/Jexytd/kodok/main/UI.lua'))()
+local kodok = UI:New(game:GetService('HttpService'):GenerateGUID(false))
+local UISettings = {
+    'keys',
+    Title = 'Made by Oyen'
+}
+local KeyUI = kodok:Setup(UISettings)
+
+if KeyUI then
+    local connection;
+    connection = KeyUI:setFocusLost(function(enterPressed, input)
+        if enterPressed then
+            local guess = KeyUI:getText();
+            if KeyUI.keys.TryAttempt <= 3 then
+                if SIMP:isOwn(KeyUI.keys.WrongKeys, guess) then
+                    print('You can\'t insert the wrong key twice!')
+                    return;
+                end
+
+                local checker = Auth(guess);
+                if checker ~= 0 then
+                    KeyUI.keys.TryAttempt = KeyUI.keys.TryAttempt + 1;
+                    table.insert(KeyUI.keys.WrongKeys, guess);
+                else
+                    KeyUI.keys.akses_granted = true; --/ Easy to bypass :|
+                    connection:Disconnect();
+                end
+            else
+                connection:Disconnect();
+            end
+        end
+    end)
+
+    local continue = false;
+    local Fail = false;
+    repeat
+        if KeyUI.keys.TryAttempt > 3 then Fail = true; end
+        if typeof(KeyUI.keys.akses_granted) == 'boolean' and KeyUI.keys.akses_granted then
+            continue = true;
+        end
+    until continue and KeyUI.keys.TryAttempt <= 3 or Fail;
+
+    if Fail then
+        print('Unable to access the next step. Requirement not met')
+        UI:Uninstall();
+        return -1;
+    end
+end
 
 -- / Send discord webhook /--
 getgenv().wSend = loadstring(SIMP:Get('https://raw.githubusercontent.com/Jexytd/kodok/main/Webhook.lua'))()
@@ -21,35 +64,12 @@ local ResponseGame = loadstring(SIMP:Get('https://raw.githubusercontent.com/Jexy
 local UNIVERSAL = false;
 if ResponseGame == 1 then
 	return 0;
+elseif ResponseGame == -1 then
+    return 1;
 elseif ResponseGame == 'UNIVERSAL' then
 	UNIVERSAL = true;
 end
 
-print('Game checker passed!')
-
-local UI = loadstring(SIMP:Get('https://raw.githubusercontent.com/Jexytd/kodok/main/UI.lua'))()
-
-local kodok = UI:New('kodok')
-
-local UIconfigs = {
-    'Loading',
-
-    Title = 'Oyen Penguasa Dunia'
-}
-local loader = kodok:Setup(UIconfigs)
-
-if loader then
-    loader:Open()
-
-    wait(1)
-
-    print('Ooo ma gaa, is mostly bugging')
-    loader:setContent('Vroom...')
-    wait(0.1)
-    loader:setProgress(0.5)
-
-    wait(1)
-    loader:Close();
-
-    kodok:Destroy()
+if UNIVERSAL then
+    print('You pass all the step!')
 end
